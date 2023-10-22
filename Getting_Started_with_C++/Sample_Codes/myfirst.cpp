bcce96c0b8d374f1231be689a893133b7c37f04e
cbhutad <learningcbhutad@gmail.com>
2023-10-23 00:13:02 +0530

    Initial cpp source code

---
+++ Getting_Started_with_C++/Sample_Codes/myfirst.cpp
@@ -0,0 +1,10 @@
+//myfirst.cpp - displays a message
+
+#include <iostream>
+
+int main() {
+    using namespace std;
+    cout << "Come up and c++ some time" << endl;
+    cout << "You won't regret it" << endl;
+    return 0;
+}
