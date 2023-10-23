57279dd4af328207a63710ee24d68443905f00ea
cbhutad <learningcbhutad@gmail.com>
2023-10-23 22:30:13 +0530

    Variable declaration and assignment sample

---
+++ Getting_Started_with_C++/Sample_Codes/carrots.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main() {
+    using namespace std;
+    int carrots;
+    carrots = 25;
+    cout << "I have ";
+    cout << carrots;
+    cout << " carrots.";
+    cout << endl;
+    carrots = carrots - 1;
+    cout << "Crunch, Crunch. Now i have " << carrots << "  carrots" << endl;
+    return 0;
+}
